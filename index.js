dbc00aec484de055b5e6dfc1e709f31b4cb17e82
popop098 <53284350+popop098@users.noreply.github.com>
2021-04-22 10:37:04 +0900

    add

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.mongoURL,{
     useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
 }).then(()=>console.log('몽고DB연결완료.')).catch(err=>console.log(err))
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! zzz')
 })
 app.post('/register',(req, res) => {
 
@@ -23,6 +23,25 @@ app.post('/register',(req, res) => {
         if(err) return res.json({success:false,err})
         return res.status(200).json({
             success:true
+        })
+    })
+
+})
+app.post('/login',(req,res)=>{
+
+    User.findOne({ email:req.body.email },(err,user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess : false,
+                message : "이메일을 찾을수가 없습니다."
+            })
+        }
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch)
+                return res.json({loginSuccess:false,message:"비밀번호가 옳지않습니다."})
+            user.generateToken((err,user)=>{
+
+            })
         })
     })
 
