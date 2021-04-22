c35f4a4f8e06f13c4f01ada755e24c034d802517
popop098 <53284350+popop098@users.noreply.github.com>
2021-04-22 09:52:20 +0900

    fix

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose')
+const {User} = require('./models/User')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 require('dotenv').config()
 mongoose.connect(process.env.mongoURL,{
     useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
@@ -9,7 +14,19 @@ mongoose.connect(process.env.mongoURL,{
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.post('/register',(req, res) => {
 
+
+    const user = new User(req.body)
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
+})
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
